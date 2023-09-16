bc248c9a07f40b1334f58de0b3a97d8bd0437da5
Andrew Lord <andrew.lord@nuggets.life>
2023-09-16 08:50:39 +0100

    :loud_sound:  Log user_id

--- src/pages/api/didcomm.ts
+++ src/pages/api/didcomm.ts
@@ -66,6 +66,7 @@ const getDuoMFA = async (username: string) => {
 
   // TODO: get user_id from user object
   const user_id = user?.response?.user_id
+  console.log('>>> DUO USER ID', user_id)
 
   const mfaResponse = await new Promise((resolve, reject) => {
     duo.jsonApiCall(
